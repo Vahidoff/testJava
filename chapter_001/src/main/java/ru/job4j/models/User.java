76dc9142596dac48876d644940d193f29747462a
Aleksundrr Vahheedofv <arbuz333@hotmail.com>
2018-01-13 14:21:57 +0300

    Adding session factory

--- chapter_001/src/main/java/ru/job4j/models/User.java
+++ chapter_001/src/main/java/ru/job4j/models/User.java
@@ -38,4 +38,38 @@ public class User {
     public void setId(int id) {
         this.id = id;
     }
+
+    /**
+     * equals.
+     * by id User
+     * @param o - Object
+     * @return boolean result of object comparison
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return this.id == user.id && this.name.equals(user.name)
+                && this.password.equals(user.password);
+    }
+
+    /**
+     * hashCode.
+     * override
+     * @return id
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+        result = prime * result + ((this.password == null) ? 0 : this.password.hashCode());
+        return result;
+    }
 }
